5359cb65577844b3a2b891cca2a119f4c558beaf
mattiavolpe <64392380+mattiavolpe@users.noreply.github.com>
2023-05-23 22:20:28 +0200

    FIX comic image position

--- resources/views/singleComic.blade.php
+++ resources/views/singleComic.blade.php
@@ -7,7 +7,7 @@ Single Comic
 @section('content')
   <div id="separator" class="bg-primary py-5">
     <div class="container position-relative">
-      <img width="200" src="{{ $db['comics'][0]['thumb'] }}" alt="" class="position-absolute start-0 bottom-50">
+      <img id="single_comic_image" width="200" src="{{ $db['comics'][0]['thumb'] }}" alt="Comic image" class="position-absolute bottom-50">
     </div>
   </div>
   @include("partials.comic_details_top")
